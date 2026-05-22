7f7ac53a758d5b9f7d89651ccd282472a588a95d
Ahnti <ahnahn444@gmail.com>
2026-05-23 01:01:12 +0800

    massive reminder

--- main.py
+++ main.py
@@ -972,6 +972,7 @@ async def ytmp3(ctx, url: str, filename: str = None):
 
 async def load_extensions():
     await bot.load_extension("cogs.fun")
+    await bot.load_extension("cogs.util")
     await bot.add_cog(Mail(bot))
 
 import asyncio
